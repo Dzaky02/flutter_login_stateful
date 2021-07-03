f843bcc32d8e342739a94b7fb84eb2da10ee0b17
Ahmad Dzaky Abrori <broridzaky2702@gmail.com>
2021-07-04 00:24:02 +0700

    Create Login Page.

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,63 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          margin: EdgeInsets.all(20.0),
+          child: Form(
+            child: Column(
+              children: [
+                emailField(),
+                passwordField(),
+                SizedBox(height: 25.0),
+                submitButton(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget emailField() {
+    return TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        labelText: 'Email Address',
+        hintText: 'you@example.com',
+      ),
+    );
+  }
+
+  Widget passwordField() {
+    return TextFormField(
+      obscureText: true,
+      decoration: InputDecoration(
+        labelText: 'Password',
+        hintText: 'Enter Password',
+      ),
+    );
+  }
+
+  Widget submitButton() {
+    return ElevatedButton(
+      onPressed: () {},
+      child: Text('Submit'),
+      style: ElevatedButton.styleFrom(
+        primary: Colors.teal,
+      ),
+    );
+  }
+}
